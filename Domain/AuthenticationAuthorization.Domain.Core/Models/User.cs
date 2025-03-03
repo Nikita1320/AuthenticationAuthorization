8c5729fd4dc3b3b5304f6dc6d286770ab83a493c
Nikita1320 <105381734+Nikita1320@users.noreply.github.com>
2025-03-03 12:17:44 +0300

    Create models

---
+++ Domain/AuthenticationAuthorization.Domain.Core/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationAuthorization.Domain.Core.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Guid UserClaimsId { get; set; }
+        public UserClaims UserClaims { get; set; }
+    }
+}
---
+++ Domain/AuthenticationAuthorization.Domain.Core/Models/UserClaims.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthenticationAuthorization.Domain.Core.Models
+{
+    public class UserClaims
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string HashPassword { get; set; } = string.Empty;
+        public User User { get; set; }
+    }
+}
