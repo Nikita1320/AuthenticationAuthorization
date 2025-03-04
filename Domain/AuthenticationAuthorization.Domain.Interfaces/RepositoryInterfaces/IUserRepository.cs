78720dd49b91fb714da534eda2b4a7bd417db81c
Nikita1320 <105381734+Nikita1320@users.noreply.github.com>
2025-03-04 14:30:05 +0300

    Added user repository interface

---
+++ Domain/AuthenticationAuthorization.Domain.Interfaces/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using AuthenticationAuthorization.Domain.Core.Models;
+
+namespace AuthenticationAuthorization.Domain.Interfaces.RepositoryInterfaces
+{
+    internal interface IUserRepository
+    {
+        public Task<List<User>> Get();
+        public Task<User> GetById(Guid id);
+        public Task<User> GetByLogin(string login);
+        public Task Add(User user);
+        public Task Update(User user);
+        public Task Delete(Guid id);
+    }
+}
