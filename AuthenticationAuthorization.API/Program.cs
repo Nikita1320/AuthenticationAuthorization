ae69d7f766a9d4723081d71b5d9e9b7f07ca2f69
Nikita1320 <105381734+Nikita1320@users.noreply.github.com>
2025-03-04 17:26:10 +0300

    Added Nlog and Swagger

--- AuthenticationAuthorization.API/Program.cs
+++ AuthenticationAuthorization.API/Program.cs
@@ -1,3 +1,7 @@
+using AuthenticationAuthorization.Infrastrucnure.Data.Contexts;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuthenticationAuthorization.API
 {
     public class Program
@@ -5,6 +9,17 @@ namespace AuthenticationAuthorization.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var configuration = builder.Configuration;
+            builder.Services.AddLogging();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationContext>(otions =>
+            {
+                otions.UseNpgsql(configuration.GetConnectionString(nameof(ApplicationContext)));
+            });
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
