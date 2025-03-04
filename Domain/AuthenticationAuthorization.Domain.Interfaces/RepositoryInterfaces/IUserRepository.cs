60fc48744c1320c8da444cbb25bdfeae9ee49aee
Nikita1320 <105381734+Nikita1320@users.noreply.github.com>
2025-03-04 17:25:33 +0300

    Corrected models and interface repository

--- Domain/AuthenticationAuthorization.Domain.Interfaces/RepositoryInterfaces/IUserRepository.cs
+++ Domain/AuthenticationAuthorization.Domain.Interfaces/RepositoryInterfaces/IUserRepository.cs
@@ -5,8 +5,8 @@ namespace AuthenticationAuthorization.Domain.Interfaces.RepositoryInterfaces
     public interface IUserRepository
     {
         public Task<List<User>> Get();
-        public Task<User> GetById(Guid id);
-        public Task<User> GetByLogin(string login);
+        public Task<User?> GetById(Guid id);
+        public Task<User?> GetByLogin(string login);
         public Task Add(User user);
         public Task Update(User user);
         public Task Delete(Guid id);
